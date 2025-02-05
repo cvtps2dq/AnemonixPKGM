af6c083cc794e95ec0b3f0e84916d7dfb15c4088
Alexey Shaforostov <kostelbeats@gmail.com>
2025-02-06 02:51:16 +0300

    fixed --bootstrap arg and deletion in bootstrap

--- main.cpp
+++ main.cpp
@@ -12,63 +12,73 @@ int main(const int argc, char* argv[]) {
 
     const std::string command = argv[1];
     bool force = false, reinstall = false, bootstrap = false;
+    std::string bootstrap_path;
     std::vector<std::string> arguments;
 
-    // Parse arguments and flags
+    // Parse arguments correctly
     for (int i = 2; i < argc; ++i) {
         std::string arg = argv[i];
         if (arg == "--force") force = true;
         else if (arg == "--reinstall") reinstall = true;
-        else if (arg == "--bootstrap") bootstrap = true;
-        else arguments.push_back(arg);
+        else if (arg == "--bootstrap") {
+            bootstrap = true;
+            if (i + 1 < argc) { // Ensure there's another argument after --bootstrap
+                bootstrap_path = argv[++i]; // Capture the path and skip next iteration
+            } else {
+                std::cerr << "err: --bootstrap flag requires a path argument\n";
+                return 1;
+            }
+        } else {
+            arguments.push_back(arg);
+        }
+    }
+
+    // Handle bootstrap mode by updating paths
+    if (bootstrap) {
+        if (bootstrap_path.empty()) {
+            std::cerr << "err: Missing bootstrap path\n";
+            return 1;
+        }
+        AConf::ANEMO_ROOT = bootstrap_path + AConf::ANEMO_ROOT;
+        AConf::BSTRAP_PATH = bootstrap_path;
+        AConf::DB_PATH = AConf::ANEMO_ROOT + "/installed.db";
     }
 
     if (command == "init") {
-        if (bootstrap && !arguments.empty()) {
-            AConf::ANEMO_ROOT = arguments[0] + AConf::ANEMO_ROOT;
-            AConf::BSTRAP_PATH = arguments[0];
-            AConf::DB_PATH = AConf::ANEMO_ROOT + "/installed.db";
-        }
         Anemo::init();
     } else if (command == "help") {
         Anemo::showHelp();
     } else if (command == "install" && !arguments.empty()) {
-        if (bootstrap && arguments.size() > 1) {
-            AConf::ANEMO_ROOT = arguments[1] + AConf::ANEMO_ROOT;
-            AConf::BSTRAP_PATH = arguments[1];
-            AConf::DB_PATH = AConf::ANEMO_ROOT + "/installed.db";
-            arguments.erase(arguments.begin()); // Remove path from package list
-        }
         for (const auto& pkg : arguments) {
             if (!Anemo::install({pkg}, force, reinstall)) {
                 std::cerr << "Failed to install: " << pkg << "\n";
             }
         }
     } else if (command == "remove" && !arguments.empty()) {
-        std::string bootstrap_path;
-        if (bootstrap && arguments.size() > 1) {
-            bootstrap_path = arguments[0];
-            AConf::ANEMO_ROOT = bootstrap_path + AConf::ANEMO_ROOT;
-            AConf::BSTRAP_PATH = bootstrap_path;
-            AConf::DB_PATH = AConf::ANEMO_ROOT + "/installed.db";
-            arguments.erase(arguments.begin()); // Remove path from package list
-        }
         for (const auto& pkg : arguments) {
             if (!Anemo::remove(pkg, force, bootstrap)) {
                 std::cerr << "Failed to remove: " << pkg << "\n";
             }
         }
     } else if (command == "audit") {
-        if (bootstrap && !arguments.empty()) {
-            AConf::ANEMO_ROOT = arguments[0] + AConf::ANEMO_ROOT;
-            AConf::BSTRAP_PATH = arguments[0];
+        if (bootstrap) {
+            if (bootstrap_path.empty()) {
+                std::cerr << "err: Missing bootstrap path\n";
+                return 1;
+            }
+            AConf::ANEMO_ROOT = bootstrap_path + AConf::ANEMO_ROOT;
+            AConf::BSTRAP_PATH = bootstrap_path;
             AConf::DB_PATH = AConf::ANEMO_ROOT + "/installed.db";
         }
         Anemo::audit();
     } else if (command == "list") {
-        if (bootstrap && !arguments.empty()) {
-            AConf::ANEMO_ROOT = arguments[0] + AConf::ANEMO_ROOT;
-            AConf::BSTRAP_PATH = arguments[0];
+        if (bootstrap) {
+            if (bootstrap_path.empty()) {
+                std::cerr << "err: Missing bootstrap path\n";
+                return 1;
+            }
+            AConf::ANEMO_ROOT = bootstrap_path + AConf::ANEMO_ROOT;
+            AConf::BSTRAP_PATH = bootstrap_path;
             AConf::DB_PATH = AConf::ANEMO_ROOT + "/installed.db";
         }
         Anemo::list();
@@ -77,5 +87,6 @@ int main(const int argc, char* argv[]) {
         Anemo::showHelp();
         return 1;
     }
+
     return 0;
 }
