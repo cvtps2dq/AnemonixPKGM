bd00066971d25a221637d3e146aaeca39bc94d59
Alexey Shaforostov <kostelbeats@gmail.com>
2025-02-20 20:25:26 +0300

    systemd crash

--- src/core/Utilities.cpp
+++ src/core/Utilities.cpp
@@ -165,7 +165,7 @@ bool Utilities::extractRemainingFiles(const std::string& package_path,
         }
 
         std::string filename = archive_entry_pathname(entry);
-        std::cout << filename << std::endl;
+        //std::cout << filename << std::endl;
 
         if (exclude_files.contains(filename)) {
             archive_read_data_skip(a);
