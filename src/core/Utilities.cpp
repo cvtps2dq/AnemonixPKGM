0a15c7ed465e4ca2a704d0926691a37cc5be9753
Alexey Shaforostov <kostelbeats@gmail.com>
2025-02-20 19:32:58 +0300

    systemd crash

--- src/core/Utilities.cpp
+++ src/core/Utilities.cpp
@@ -96,9 +96,8 @@ bool Utilities::extractMetadataAndScripts(const std::string& package_path, const
         std::cout << filename << std::endl;
         std::cout << root_path << std::endl;
         std::string relative_path;
-        relative_path = filename.substr(root_path.length());
 
-        if (!isMetadataOrScript(relative_path)) {
+        if (!isMetadataOrScript(filename)) {
             archive_read_data_skip(a);
             continue;
         }
